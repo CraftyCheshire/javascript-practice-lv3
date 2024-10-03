4a58867e24a047955092a40830aee0d3810cae77
craftycheshire <mini_vann@ymail.com>
2024-10-02 19:27:41 -0500

    day one almost done

--- week-1/00-start-here-example-output.js
+++ week-1/00-start-here-example-output.js
@@ -1,4 +1,4 @@
-import { createCircle, Circle, createDog, Dog } from './objects.js';
+import { createCircle, Circle, createDog, Dog } from './00-start-here-example-objects.js';
 
 // Instances created using the factory functions
 const circle1 = createCircle(5);
@@ -10,8 +10,23 @@ console.log(dog1.bark()); // Output: "Buddy says woof!"
 
 // Instances created using the classes
 const circle2 = new Circle(7);
+const circle3 = new Circle(13);
+const circle4 = new Circle(23);
+const circle5 = new Circle(69);
 const dog2 = new Dog("Max", "German Shepherd");
+const dog3 = new Dog("Rex", "Bull-Jack");
+const dog4 = new Dog("Duke", "Mountain Cur");
+const dog5 = new Dog("Daisy", "American Pit Bull Terrier");
 
 console.log(circle2.getArea()); // Output: 153.93804002589985
 console.log(circle2.getCircumference()); // Output: 43.982297150257104
+console.log(circle3.getArea());
+console.log(circle3.getCircumference());
+console.log(circle4.getArea());
+console.log(circle4.getCircumference());
+console.log(circle5.getArea());
+console.log(circle5.getCircumference());
 console.log(dog2.bark()); // Output: "Max says woof!"
+console.log(dog3.bark()); // Output: "Rex says woof!"
+console.log(dog4.bark()); // Output: "Duke says woof!"
+console.log(dog5.bark()); // Output: "Daisy says woof!"
--- week-1/everyday-objects-run.js
+++ week-1/everyday-objects-run.js
@@ -1,7 +1,27 @@
-import { Car } from "everyday-objects.js";
+import { Car } from "./everyday-objects.js";
+import { Phone } from "./everyday-objects.js";
+import { Book } from "./everyday-objects.js";
+import { Laptop } from "./everyday-objects.js";
+import { Backpack } from "./everyday-objects.js";
 
 // use node everyday-objects.js to try this out.
 // Create instances
 const myCar = new Car("Toyota", "Corolla", 2020);
 console.log(myCar.getCarInfo()); // Output: "2020 Toyota Corolla"
 myCar.startEngine(); // Output: "The engine of Toyota Corolla is now running."
+
+const myPhone = new Phone("Nokia", "3310", 100);
+console.log(myPhone.makeCall(9856626702));
+myPhone.batteryStatus();
+
+const myBook = new Book("The Hitchhiker's Guide to the Galaxy", "Douglas Adams", 815);
+console.log(myBook.getSummary());
+myBook.readPages();
+
+const myLaptop = new Laptop("Lenovo", "Yoga 730-15IKB", 16);
+console.log(myLaptop.bootUp());
+myLaptop.shutDown();
+
+const myBackpack = new Backpack("SwissGear", "Black", 31);
+console.log(myBackpack.open);
+myBackpack.checkCapacity();
